2383e625cfa820d23c1c6356f6d78935320740ac
D3stinn3 <paynedestinne@gmail.com>
2023-11-15 23:26:35 +0300

    Admin Model Registration

--- paytask/admin.py
+++ paytask/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from paytask.models import Teacher, Student, Performance
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Performance)
